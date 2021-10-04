f983bf38358b565531cc87409ce20afea5390207
Ricardo Salazar <no-replay>
2021-10-04 03:56:22 -0500

    fixed login and singup forms

--- Develop/client/src/components/LoginForm.js
+++ Develop/client/src/components/LoginForm.js
@@ -1,9 +1,9 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-import { loginUser } from '../utils/mutations';
 import Auth from '../utils/auth';
 import { useMutation } from '@apollo/react-hooks';
+import { LOGIN_USER } from '../utils/mutations';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
